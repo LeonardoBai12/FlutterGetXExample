76d89e457a8525252b5f11314eb3e282f9049394
LeonardoBai12 <69223422+LeonardoBai12@users.noreply.github.com>
2022-02-07 09:58:42 -0300

    Adicionada Splash e Ícones

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:new_app_getx/controller/todo_controller.dart';
-import 'package:new_app_getx/models/todo.dart';
 import 'package:new_app_getx/screens/todo_screen.dart';
 
 class HomeScreen extends StatefulWidget {
